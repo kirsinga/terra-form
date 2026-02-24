98db4d9650d3c3652fa54cd2e4e9e0a0dd55271a
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-23 22:33:22 -0600

    changes in the datasource

--- DemoDataSource/createInstance .tf
+++ DemoDataSource/createInstance .tf
@@ -1,27 +1,13 @@
 
-data "aws_availability_zones" "available" {
-  
-}
-data "aws_ami" "latest" {
-  most_recent = false
-  filter {
-    name   = "name"
-   values = {"ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"}
-  }  
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-    owners = ["099720109477"] # Amazon
-}
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = data.aws_ami.latest.id
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
  
   tags = {
     Name = "custom_instance"
-  }
+    
+    }
 
    
 }
---
+++ DemoDataSource/securitygroup.tf
@@ -0,0 +1,28 @@
+data "aws_ip_ranges" "current" {
+  regions = ["us-east-1"]
+  services = ["ec2"]
+}
+resource "aws_security_group" "sg-custome_us_east" {
+  name        = "sg-custome_us_east"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = data.aws_ip_ranges.current.cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    CreateDate = data.aws_ip_ranges.current.create_date
+    SyncToken  = data.aws_ip_ranges.current.sync_token
+  }
+
+}
