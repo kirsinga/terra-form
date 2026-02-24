6d23bfa5760a50374591286c3d0db2ce13a69d21
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-23 22:57:45 -0600

    changes for datasource

--- DemoDataSource/createInstance .tf
+++ DemoDataSource/createInstance .tf
@@ -3,11 +3,16 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
- 
+
+  provisioner "local-exec" {
+      command = "echo ${aws_instance.MyFirstInstnace.public_ip} > instance_ip.txt" 
   tags = {
     Name = "custom_instance"
     
     }
+  output "instance_ip" {
+    value = aws_instance.MyFirstInstnace.public_ip
+  }
 
    
 }
