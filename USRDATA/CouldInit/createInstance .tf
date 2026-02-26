b0fbca9d7b35a4e5f9f1dafa3d5a1745433c902b
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-26 13:32:52 -0600

    changes for CouldInt

--- USRDATA/CouldInit/createInstance .tf
+++ USRDATA/CouldInit/createInstance .tf
@@ -8,7 +8,9 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.levelup_key.key_name
-  user_data     = file("installapache.sh")
+  user_data     = data.template_cloudinit_config.intall-apache-config.rendered
+
+
 
   tags = {
     Name = "custom_instance"
