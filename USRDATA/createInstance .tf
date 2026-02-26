99305c25194451728f364e40141438ae4b914631
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-25 20:29:58 -0600

    changes for USDATA

--- USRDATA/createInstance .tf
+++ USRDATA/createInstance .tf
@@ -8,21 +8,12 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.levelup_key.key_name
+  user_data     = file("installapache.sh")
 
   tags = {
     Name = "custom_instance"
   }
 }
-resource "aws_volume_attachment" "ebs_attachement" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.levelup_volume.id
-  instance_id = aws_instance.MyFirstInstnace.id
-
-}
-resource "aws_ebs_volume" "levelup_volume" {
-  availability_zone = aws_instance.MyFirstInstnace.availability_zone
-  size              = 50
-  tags = {
-    Name = "secondary_volume"
-   }
+output "public_ip" {
+  value = aws_instance.MyFirstInstnace.public_ip
 }
