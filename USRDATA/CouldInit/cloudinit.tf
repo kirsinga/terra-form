3ba41a8441eb4939770bc4bd6c37af0327c96a55
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-26 13:23:53 -0600

    changes for cloud

---
+++ USRDATA/CouldInit/cloudinit.tf
@@ -0,0 +1,14 @@
+data "template_file" "cloudinit" {
+  template = file("${path.module}/init.cfg")
+}
+
+data "template_cloudinit_config" "intall-apache-config" {
+    gzip = true
+    base64_encode = true
+
+    part {
+      filename = "init.cfg"
+      content_type = "text/could-config"
+      content = data.template_file.cloudinit.rendered
+       }
+}
