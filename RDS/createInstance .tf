e2ccdb1c9099abfa4e0c44319c80e1e7a7c316a4
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-26 16:55:57 -0600

    changes for RDS

--- RDS/createInstance .tf
+++ RDS/createInstance .tf
@@ -8,7 +8,7 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.levelup_key.key_name
-  vpc_security_group_ids = [aws_security_group.allow_mariadb.id]
+  vpc_security_group_ids = [aws_security_group.levelup_allow_ssh.id]
   subnet_id = aws_subnet.levelup_subnet1.id
 
 
