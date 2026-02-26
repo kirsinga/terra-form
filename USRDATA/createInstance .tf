7ce66c4b961b5ca1d42850346d5a1717fdd284b6
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-25 20:42:30 -0600

    changes for Data

--- USRDATA/createInstance .tf
+++ USRDATA/createInstance .tf
@@ -1,13 +1,12 @@
 
-resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
+data "aws_key_pair" "levelup_key" {
+  key_name = var.KEY_PAIR_NAME
 }
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.levelup_key.key_name
+  key_name      = data.aws_key_pair.levelup_key.key_name
   user_data     = file("installapache.sh")
 
   tags = {
