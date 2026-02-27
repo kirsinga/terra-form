ebbdec114f0e710f229c2203442198cc90128d78
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-26 19:19:29 -0600

    changes for IAM

--- IAMNONADMINUSER/iamroles.tf
+++ IAMNONADMINUSER/iamroles.tf
@@ -1,5 +1,5 @@
 //rolls to access s3 bucket
-resource "aws_iam_role" "levelup_s3_access_role" {
+resource "aws_iam_role" "levelup-s3-access-role" {
   name = "levelup_s3_access_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -16,8 +16,8 @@ resource "aws_iam_role" "levelup_s3_access_role" {
   })
 }
 //policy  to attach the  s3 Bucket  role
-resource "aws_iam_role_policy" "levelup_s3_access_attachment" {
-  role  = aws_iam_role.levelup_s3_access_role.name
+resource "aws_iam_role_policy" "levelup-s3-access-attachment" {
+  role  = aws_iam_role.levelup-s3-access-role.name
   policy= jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -27,8 +27,8 @@ resource "aws_iam_role_policy" "levelup_s3_access_attachment" {
           "s3:*"
                 ]
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.levelup_bucket.id}",
-          "arn:aws:s3:::${aws_s3_bucket.levelup_bucket.id}/*"
+          "arn:aws:s3:::${aws_s3_bucket.levelup-bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.levelup-bucket.id}/*"
         ]
       }
     ]
@@ -36,7 +36,7 @@ resource "aws_iam_role_policy" "levelup_s3_access_attachment" {
   
 }
 //instace Identifier
-resource "aws_iam_instance_profile" "levelup_s3_access_instance_profile" {
-  name = "levelup_s3_access_instance_profile"
-  role = aws_iam_role.levelup_s3_access_role.name
+resource "aws_iam_instance_profile" "levelup-s3-access-instance-profile" {
+  name = "levelup-s3-access-instance-profile"
+  role = aws_iam_role.levelup-s3-access-role.name
 }
--- IAMNONADMINUSER/s3bucket.tf
+++ IAMNONADMINUSER/s3bucket.tf
@@ -1,5 +1,5 @@
 //create AWS S3 bucket
-resource "aws_s3_bucket" "levelup_bucket" {
+resource "aws_s3_bucket" "levelup-bucket" {
   bucket = "levelup-bucket-terraform"
   tags = {
     name = "levelup-bucket-terraform"
