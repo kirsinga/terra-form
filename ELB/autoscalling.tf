2fd1fb1f26d3dc0a10901756ddbc6ab37e0d64cb
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-27 19:32:32 -0600

    changes for ELB

--- ELB/autoscalling.tf
+++ ELB/autoscalling.tf
@@ -49,4 +49,33 @@ resource "aws_autoscaling_group" "levelup_asg" {
 output "elb_dns_name" {
   value = aws_elb.levelupelb.dns_name
 }
+//security group for EC2 instances
+resource "aws_security_group" "ec2_sg_instance" {    
+    name        = "ec2_sg_instance"
+    description = "Allow HTTP traffic from ELB to EC2 instances"
+    vpc_id      = aws_vpc.vpc_levelup.id
+    
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+      
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        security_groups = [aws_security_group.elb_sg.id]
+    }
+    egress  {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
+    tags ={
+        Name = "ec2_sg_instance"
+    }
+}
