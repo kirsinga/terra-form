4aafd48bb51c29c443011312db0633a05e5671cc
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-28 16:10:30 -0600

    changes for ELB

--- Module/main.tf
+++ Module/main.tf
@@ -3,7 +3,7 @@ module "ec2_cluster" {
   source = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 5.0"
   name = "ec2_cluster"
-  ami = "ami-0f40c8f97004632f9"
+  ami = "ami-05692172625678b4e"
   instance_type = "t2.micro"
   subnet_id = "subnet-05e41af1b0abba5d"
 
