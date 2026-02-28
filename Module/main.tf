f367c4d4f0872c9bfdf30d1c9b045b06cbd0178d
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-28 16:16:22 -0600

    changes for ELB

--- Module/main.tf
+++ Module/main.tf
@@ -1,11 +1,18 @@
 //define madule
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
 module "ec2_cluster" {
   source = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 5.0"
   name = "ec2_cluster"
   ami = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
-  subnet_id = "subnet-05e41af1b0abba5d"
+  subnet_id = data.aws_subnets.default_subnets.ids[0]
 
    tags = {
     Name = "ec2_cluster"
