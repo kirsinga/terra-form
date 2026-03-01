6dc9b18361f73e4051170be5f1e03a35d899b5a6
kiransingampalli <kiransingampalliaws@gmail.com>
2026-02-28 20:51:03 -0600

    changes for ELB

--- Module-VPC/module/variable.tf
+++ Module-VPC/module/variable.tf
@@ -7,7 +7,7 @@ variable "subnet1_az" {
 variable "public_key_path" {
     description = "The path to the public key file"
     type        = string
-    default     = "~/.ssh/levelup.pub"
+    default     = "~/.ssh/levelup_key.pub"
     }
    variable "ami_id" {
     description = "The AMI ID for the EC2 instance"
