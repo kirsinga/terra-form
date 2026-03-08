b5c84bfd11e45274174a4015355ec84211aa2e54
kiransingampalli <kiransingampalliaws@gmail.com>
2026-03-08 07:46:44 -0500

    changes for Deployment1

--- casestudy/provider.tf
+++ casestudy/provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source = "hashicorp/http"
+    }
+  }
+}
+
 provider "aws" {
   region = var.AWS_REGION
 }
